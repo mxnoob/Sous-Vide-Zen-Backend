96100fca2a8eeaa0ed1fb1f45c584ce0d3d8a469
vbamnup <vbamnup@gmail.com>
2024-03-28 20:04:49 +0300

    add test throttling

--- src/tests/base/test_validator.py
+++ src/tests/base/test_validator.py
@@ -1,6 +1,13 @@
 import pytest
-from django.core.exceptions import ValidationError
 from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+from src.base.validators import (
+    CustomPasswordValidator,
+    NoLowerCaseValidator,
+    NoUpperCaseValidator,
+    NoNumbersValidator,
+)
 
 
 @pytest.mark.parametrize(
@@ -31,3 +38,29 @@ def test_password_validators(password):
         # Для всех остальных паролей ожидаем ValidationError
         with pytest.raises(ValidationError):
             validate_password(password)
+
+
+@pytest.mark.parametrize(
+    "validator_class, expected_help_text",
+    [
+        (
+            CustomPasswordValidator,
+            "Пароль должен состоять не менее чем из 8 символов: минимум с одной прописной (a-z) и одной заглавной буквой (A-Z), минимум с одной цифрой (0-9) и одним специальным символом (!@#$%^&*).",
+        ),
+        (
+            NoLowerCaseValidator,
+            "Пароль должен содержать хотя бы одну строчную букву (a-z, а-я).",
+        ),
+        (
+            NoUpperCaseValidator,
+            "Пароль должен содержать хотя бы одну заглавную букву (A-Z, А-Я).",
+        ),
+        (NoNumbersValidator, "Пароль должен содержать хотя бы одну цифру (0-9)."),
+    ],
+)
+def test_validator_help_texts(validator_class, expected_help_text):
+    validator = validator_class()
+    help_text = validator.get_help_text()
+    assert (
+        help_text == expected_help_text
+    ), f"{validator_class.__name__} help text did not match expected"
