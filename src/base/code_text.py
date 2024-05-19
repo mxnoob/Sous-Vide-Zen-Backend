aa978437045125c9d83410ae07696eae23408334
roman <romaxid@yandex.by>
2024-05-19 11:40:46 +0300

    fix: some little changes.

--- src/base/code_text.py
+++ src/base/code_text.py
@@ -13,7 +13,7 @@ COMMENT_NOT_FOUND: dict = {"detail": "Комментарий не найден."
 COMMENT_SUCCESSFULLY_DELETE: dict = {"message": "Комментарий удален!"}
 CANT_EDIT_COMMENT: dict = {
     "detail": "Обновление комментария возможно только в течение суток после "
-              "создания."
+    "создания."
 }
 
 SUCCESSFUL_APPRECIATED_COMMENT: dict = {"message": "Вы оценили комментарий!"}
@@ -25,12 +25,8 @@ PASSWORDS_IS_NOT_SIMILAR: dict = {"password": "Пароли не совпада�
 # Author status
 AUTHOR_IS_MISSING: dict = {"message": "Отсутствует автор."}
 AUTHOR_NOT_FOUND: dict = {"message": "Автор не найден."}
-SUCCESSFUL_ATTEMPT_ON_AUTHOR: dict = {
-    "message": "Вы успешно подписались на автора."
-}
-NOT_FOLLOWING_THIS_USER: dict = {
-    "detail": "Вы не подписаны на этого пользователя."
-}
+SUCCESSFUL_ATTEMPT_ON_AUTHOR: dict = {"message": "Вы успешно подписались на автора."}
+NOT_FOLLOWING_THIS_USER: dict = {"detail": "Вы не подписаны на этого пользователя."}
 SUCCESSFUL_UNSUBSCRIBE_FROM_THE_AUTHOR: dict = {
     "message": "Вы успешно отписались от автора."
 }
@@ -43,28 +39,21 @@ REACTION_ALREADY_SET: dict = {"detail": "Вы уже поставили таку
 SUCCESSFUL_RATED_IT: dict = {"message": "Вы поставили оценку!"}
 SUCCESSFUL_LIKED_THE_RECIPE: dict = {"message": "Вы оценили рецепт!"}
 REACTION_CANCELLED: dict = {"message": "Реакция отменена!"}
-ALREADY_RATED_THIS_COMMENT: dict = {
-    "detail": "Вы уже оценили данный комментарий."
-}
+ALREADY_RATED_THIS_COMMENT: dict = {"detail": "Вы уже оценили данный комментарий."}
 SUCCESSFUL_RATED_COMMENT: dict = {"message": "Вы оценили комментарий!"}
 
 # Recipes status
 SUCCESSFUL_APPRECIATED_RECIPE: dict = {"message": "Вы оценили рецепт!"}
 RECIPE_SUCCESSFUL_DELETE: dict = {"message": "Рецепт успешно удален."}
-RECIPE_ALREADY_IN_FAVORITES: dict = {
-    "detail": "Рецепт уже находится в избранном."
-}
+RECIPE_ALREADY_IN_FAVORITES: dict = {"detail": "Рецепт уже находится в избранном."}
 SUCCESSFUL_ADDED_TO_FAVORITES: dict = {"detail": "Рецепт добавлен в избранное."}
-THE_RECIPE_IS_NOT_IN_FAVORITES: dict = {
-    "detail": "Рецепт не находится в избранном."
-}
+THE_RECIPE_IS_NOT_IN_FAVORITES: dict = {"detail": "Рецепт не находится в избранном."}
 RECIPE_REMOVED_FROM_FAVORITES: dict = {"detail": "Рецепт удален из избранного."}
 THE_LIST_OF_FAVORITES_RECIPES_IS_EMPTY: dict = {
     "detail": "Список избранных рецептов пуст."
 }
 RECIPE_CAN_BE_EDIT_ONLY_ONCE_PER_DAY: dict = {
-    "detail": "Обновление рецепта возможно только в течение суток после "
-              "создания."
+    "detail": "Обновление рецепта возможно только в течение суток после " "создания."
 }
 
 # Errors status
